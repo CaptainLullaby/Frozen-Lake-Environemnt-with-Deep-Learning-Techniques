bc2a4125053a67e53af99464cf295739e9aece4b
MoonFace.ExE <grndvl007@gmail.com>
2022-12-27 15:09:37 +0000

    AddedTest function to test out the environment

---
+++ test.py
@@ -0,0 +1,10 @@
+from Code import *
+    # Small lake
+lake = [['&', '.', '.', '.'],
+        ['.', '#', '.', '#'],
+        ['.', '.', '.', '#'],
+        ['#', '.', '.', '$']]
+
+env = FrozenLake(lake, slip=0.1, max_steps=16, seed=0)
+gamma = 0.5
+play(env)
