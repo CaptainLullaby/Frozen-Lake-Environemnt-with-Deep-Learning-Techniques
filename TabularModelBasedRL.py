1d0c29ee4079759c34369269fc19efeca1ca8ce6
MoonFace.ExE <grndvl007@gmail.com>
2022-12-30 18:15:16 +0000

    Updated Tabularbased
    Edited code foe TabularBased Aglos and now are in working order to find optimum policy

--- TabularModelBasedRL.py
+++ TabularModelBasedRL.py
@@ -4,32 +4,31 @@ from Epsilongreedy import *
 def policy_evaluation(env, policy, gamma, theta, max_iterations = 0):
     value = np.zeros(env.n_states, dtype=np.float)
     
-    states = env.n_states
+    iter = 0
     
-    while True:
+    while iter < max_iterations:
         delta = 0
         for s in range(env.n_states):
             v = value[s]
-            value[s] = sum([env.p(s, ns, policy[s]) * (env.r(s, ns, policy[s]) + gamma * value[ns]) for ns in range(env.n_states)])
+            value[s] = sum([env.p(ns, s, policy[s]) * (env.r(ns, s, policy[s]) + gamma * value[ns]) for ns in range(env.n_states)])
             delta = max(delta, np.abs(v - value[s]))
             
-            if delta < theta:
-                return value
+        if delta < theta:
+            return value
+        
+        iter +=1
     
-def policy_improvement(env, value, gamma):
-    
-    policy = np.zeros(env.n_states, dtype=int)
+def policy_improvement(env, policy, value, gamma):
     
     flag = True
     for s in range(env.n_states):
         action = env.a(s)
         p = policy[s]
-        policy[s] = action[np.argmax([ sum([ env.p(s, ns, a) * (env.r(s, ns, a) + gamma * value[ns]) for ns in range(env.n_states)]) for a in action])]
-        
+        policy[s] = action[np.argmax([ sum([ env.p(ns, s, a) * (env.r(ns, s, a) + gamma * value[ns]) for ns in range(env.n_states)]) for a in range(env.n_actions)])]
         if p != policy[s]:
             flag = False
 
-    return policy
+    return flag
     
 def policy_iteration(env, gamma, theta, max_iterations, policy=None):
     value = np.zeros(env.n_states)
@@ -42,8 +41,8 @@ def policy_iteration(env, gamma, theta, max_iterations, policy=None):
     flag = False
     
     while not flag:
-        value = policy_evaluation(env, policy, gamma, theta)
-        flag = policy_improvement(env, value, gamma)
+        value = policy_evaluation(env, policy, gamma, theta, max_iterations)
+        flag = policy_improvement(env, policy, value, gamma)
         
     return policy, value
     
@@ -55,20 +54,21 @@ def value_iteration(env, gamma, theta, max_iterations, value=None):
         value = np.array(value, dtype=np.float)
         
     policy = np.zeros(env.n_states, dtype = int)
+    iter = 0
     
     
-    
-    while True:
+    while iter < max_iterations:
         delta = 0
         for s in range(env.n_states):
             v = value[s]
-            value[s] = max([sum([env.p(s, ns, a) * (env.r(s, ns, a) + gamma * value[ns]) for ns in range(env.n_states)]) for a in range(4)])
+            value[s] = max([sum([env.p(ns, s, a) * (env.r(ns, s, a) + gamma * value[ns]) for ns in range(env.n_states)]) for a in range(env.n_actions)])
             delta = max(delta, abs(v- value[s]))
         
         if delta < theta:
             break
+        iter += 1
     
     for s in range(env.n_states):
-        policy[s] = np.argmax([sum([env.p(s, ns, a) * (env.r(s, ns, a) + gamma * value[s]) for ns in range(env.n_states)]) for a in range(4)])
+        policy[s] = np.argmax([sum([env.p(ns, s, a) * (env.r(ns, s, a) + gamma * value[ns]) for ns in range(env.n_states)]) for a in range(env.n_actions)])
         
     return policy, value
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from FrozenLakeEnvironment import *
 from TabularModelFreeRL import *
 from TabularModelBasedRL import *
+from NonTabularModelFreeRL import *
 
 
 #big lake
@@ -26,23 +27,31 @@ env = FrozenLake(lake, slip=0.1, max_steps=16, seed=0)
 gamma = 0.5
 #play(env)
 
-#print('## Sarsa')
+print('## Sarsa')
 max_episodes = 4000
 #policy, value = sarsa(env, max_episodes, eta=0.5, gamma=gamma, epsilon=0.5, seed=seed)
 #env.render(policy, value)
 
 print('')
 
-print('## Q-learning')
+#print('## Q-learning')
 #policy, value = q_learning(env, max_episodes, eta=0.5, gamma=gamma, epsilon=0.5, seed=seed)
 #env.render(policy, value)
 
-print('## Policy iteration')
+#print('## Policy iteration')
 #policy, value = policy_iteration(env, gamma, theta=0.001, max_iterations=128)
 #env.render(policy, value)
 
 print('')
 
-print('## Value iteration')
-policy, value = value_iteration(env, gamma, theta=0.001, max_iterations=128)
-env.render(policy, value)
+#print('## Value iteration')
+#policy, value = value_iteration(env, gamma, theta=0.001, max_iterations=128)
+#env.render(policy, value)
+
+linear_env = LinearWrapper(env)
+
+print('## Linear Sarsa')
+
+parameters = linear_sarsa(linear_env, max_episodes, eta=0.5, gamma=gamma, epsilon=0.5, seed=seed)
+policy, value = linear_env.decode_policy(parameters)
+linear_env.render(policy, value)
