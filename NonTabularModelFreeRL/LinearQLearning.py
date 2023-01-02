6992cad9b58f2dc3263705fa43305f27d0c8f655
MoonFace.ExE <grndvl007@gmail.com>
2023-01-02 13:37:10 +0000

    Update: LinearQLearning
    Added code to linear q learning, futher testing needed to make the code more viable

--- NonTabularModelFreeRL/LinearQLearning.py
+++ NonTabularModelFreeRL/LinearQLearning.py
@@ -9,10 +9,28 @@ def linear_q_learning(env, max_episodes, eta, gamma, epsilon, seed=None):
     epsilon = np.linspace(epsilon, 0, max_episodes)
     
     theta = np.zeros(env.n_features)
+    gamma_decay = np.linspace(gamma, 0, max_episodes)
+    
     
     for i in range(max_episodes):
         features = env.reset()
+        q = features.dot(theta)
+        a = epgreedy(epsilon[i], q)
+        e = np.zeros(env.n_features)
+        done  = False
         
-        # TODO:
+        while not done:
+            features_, r, done = env.step(a)
+            a_ = epgreedy(epsilon[i], q)
+            ap = epgreedy(epsilon[i], a_)
+            e = e + features[a]
+            theta = theta + eta[i] * (r + gamma * q[ap] - q[a]) * e
+            if ap == a_: 
+                e = gamma_decay[i] * e + features[a]
+            else:
+                e = 0
+            a = a_
+            features = features_
+
 
     return theta 
--- test.py
+++ test.py
@@ -10,43 +10,11 @@ lake = [['&', '.', '.', '.'],
 seed = 0
 env = FrozenLake(lake, slip=0.1, max_steps=16, seed=0)
 gamma = 0.5
-max_episodes = 4000
-#play(env)
-
-print('## Policy iteration')
-policy, value = policy_iteration(env, gamma, theta=0.001, max_iterations=128)
-env.render(policy, value)
-
-print('')
-
-print('## Value iteration')
-policy, value = value_iteration(env, gamma, theta=0.001, max_iterations=128)
-env.render(policy, value)
-
-print('')
-
-print('# Model-free algorithms')
-max_episodes = 4000
-
-print('')
-
-print('## Sarsa')
-policy, value = sarsa(env, max_episodes, eta=0.5, gamma=gamma, epsilon=0.5, seed=seed)
-env.render(policy, value)
-
-print('')
-
-print('## Q-learning')
-
-policy, value = q_learning(env, max_episodes, eta=0.5, gamma=gamma, epsilon=0.5, seed=seed)
-env.render(policy, value)
-print('')
-
-
+max_episodes = 5000
 linear_env = LinearWrapper(env)
 
-print('## Linear Sarsa')
+print('## Linear Q-learning')
 
-parameters = linear_sarsa(linear_env, max_episodes, eta=0.5, gamma=gamma, epsilon=0.5, seed=seed)
+parameters = linear_q_learning(linear_env, max_episodes, eta=0.5, gamma=gamma, epsilon=0.5, seed=seed)
 policy, value = linear_env.decode_policy(parameters)
 linear_env.render(policy, value)
